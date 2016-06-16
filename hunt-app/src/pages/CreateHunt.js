8b3ca797c08f9ff185bcf1b0ef59c52cc711ea46
morgen-montgomery <mfett004@gmail.com>
2016-06-16 03:52:25 -0700

    code cleanup

--- hunt-app/src/pages/CreateHunt.js
+++ hunt-app/src/pages/CreateHunt.js
@@ -1,47 +1,48 @@
-  import React from 'react';
+import React from 'react';
 import DocumentTitle from 'react-document-title';
+import {Button, Icon, Row, Input, Col, Card} from 'react-materialize';
 
 export default class CreateHunt extends React.Component {
 
   onSubmit (e) {
-     e.preventDefault();
-     $.ajax({
-       type: 'POST',
-       url: '/api/hunts',
-       data: $('#huntForm').serialize()
-     });
-   };
+    e.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: '/api/hunts',
+      data: $('#huntForm').serialize()
+    });
+  };
 
   render () {
     return (
-      <div className={"row"}>
+      <Row>
         <div>
-          <h4 className={"blue-text"}> Create a Scavenger Hunt </h4>
+          <h3>Create a Scavenger Hunt</h3>
         </div>
-        <div className={"row"}>
-          <form id="huntForm" className={"col m3"} onSubmit={this.onSubmit} method="post">
+        <Card>
+          <form id="huntForm" onSubmit={this.onSubmit} method="post">
             <label> Hunt Name
-            <input type="text" name="hunt_name" />
+              <input type="text" name="hunt_name" />
             </label>
             <label> Hunt Date
-            <input type="date" name="date"/>
+              <input type="date" name="date"/>
             </label>
             <label> Start Time
-            <input type="time" name="start_time"/>
+              <input type="time" name="start_time"/>
             </label>
             <label> End Time
-            <input type="time" name="end_time"/>
+              <input type="time" name="end_time"/>
             </label>
             <label> Location
-            <input type="text" name="location"/>
+              <input type="text" name="location"/>
             </label>
             <label> Description
-            <input type="text" name="description"/>
+              <input type="text" name="description"/>
             </label>
             <button> Submit </button>
           </form>
-        </div>
-      </div>
+        </Card>
+      </Row>
     )
   };
 }
