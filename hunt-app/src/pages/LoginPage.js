96b3856220fd80ad3b76e68c16e4c8536cd1e1b1
Dustin Wurtz <dtwurtz@gmail.com>
2016-06-17 12:13:44 -0700

    Form Validation!!!

--- hunt-app/src/pages/LoginPage.js
+++ hunt-app/src/pages/LoginPage.js
@@ -2,8 +2,23 @@ import React from 'react';
 import DocumentTitle from 'react-document-title';
 import { LoginForm } from 'react-stormpath';
 import {Button, Icon, Row, Input, Col, Card} from 'react-materialize';
+import ToggleDisplay from 'react-toggle-display';
 
 export default class LoginPage extends React.Component {
+  onFormSubmit(e, next) {
+     var data = e.data;
+
+    // Require passwords to be at least 10 characters.
+    if (data.password.length < 10) {
+      return next(new Error('Password must be at least 10 characters long.'));
+    }
+
+    // Force usernames to be in lowercase.
+    data.username = data.username.toLowerCase();
+
+    next(null, data);
+  }
+
   render() {
     return (
       <Row>
@@ -16,6 +31,9 @@ export default class LoginPage extends React.Component {
               <h3> Login </h3>
               <hr />
               <form id="signin-form">
+
+                <p className="alert alert-danger" spIf="form.error"><span spBind="form.errorMessage" /></p>
+
                 <Input m={12} label="First Name" />
                 <Input m={12} type="email" label="Email" />
                 <Input m={12} type="password" label="Password" />
@@ -40,6 +58,7 @@ export default class LoginPage extends React.Component {
             </Card>
           </Col>
         </Row>
+        <LoginForm onSubmit={this.onFormSubmit.bind(this)} />
       </Row>
     );
   }
